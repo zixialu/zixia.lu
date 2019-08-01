179158dcaaee82744ee4ecad6fdac7be899a1fe9
Zixia Lu <zixia@brokerbay.ca>
2019-08-01 12:19:18 -0400

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Zixia Lu`,
+    siteUrl: `https://zixia.lu`,
+    description: `Full-stack Web Developer`,
+    author: `@zixialu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
