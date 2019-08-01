9e72c433c3cb78776da5651d8db7a0fe3fa61e57
Zixia Lu <zixia@brokerbay.ca>
2019-08-01 16:43:40 -0400

    Fix imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import About from '../components/about';
-import Projects from '../components/about';
-import Contact from '../components/about';
+import Projects from '../components/projects';
+import Contact from '../components/contact';
 
 const IndexPage = () => (
   <Layout>
