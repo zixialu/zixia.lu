6c0eb05519515750fa6b0bb7ebcfbb9aa943b766
Zixia Lu <zixialu@gmail.com>
2019-08-12 17:49:53 -0400

    Remove header, add sidebar

---
+++ src/components/sidebar.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types"
+import React from "react"
+
+import './sidebar.scss';
+
+const Sidebar = () => (
+  <div>Sidebar</div>
+);
+
+Sidebar.propTypes = {
+};
+
+Sidebar.defaultProps = {
+};
+
+export default Sidebar;
