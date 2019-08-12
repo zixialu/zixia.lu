8da3c65d039020715331e6d2e2dab95d90a55bde
Zixia Lu <zixialu@gmail.com>
2019-08-12 18:00:53 -0400

    Lint

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Sidebar from '../components/sidebar';
-import "./layout.scss"
+import Sidebar from './sidebar';
+import './layout.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
@@ -30,16 +30,16 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
+          {' '}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 import About from '../components/about';
 import Projects from '../components/projects';
 import Contact from '../components/contact';
@@ -14,6 +14,6 @@ const IndexPage = () => (
     <Projects />
     <Contact />
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
