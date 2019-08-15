8551b4530d07395c6ad71f350cebe97aeffec223
Zixia Lu <zixialu@gmail.com>
2019-08-14 21:56:22 -0400

    Add scroll indicator

---
+++ src/components/scrollIndicator.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useCallback } from 'react';
+import useEventListener from '../hooks/useEventListener';
+
+import './scrollIndicator.scss';
+
+const ScrollIndicator = () => {
+  // Calculate the progress and return as a percentage string
+  const getProgress = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const winHeight = scrollHeight - clientHeight;
+    const scrolled = `${(scrollTop / winHeight) * 100}%`;
+    return scrolled;
+  };
+
+  // Track the progress as a percentage string
+  const [progress, setProgress] = useState('0%');
+
+  // Event handler to update the progress; uses useCallback so that reference
+  // never changes.
+  const scrollHandler = useCallback(
+    () => {
+      const newProgress = getProgress();
+      setProgress(newProgress);
+    },
+    [setProgress],
+  );
+
+  // Add event listener using our hook
+  useEventListener('scroll', scrollHandler);
+
+  // Set the bar's width based on progress state
+  const progressStyle = { width: progress };
+
+  return (
+    <div className="scroll-indicator">
+      <div className="scroll-fill" style={progressStyle} />
+    </div>
+  );
+};
+
+ScrollIndicator.propTypes = {
+};
+
+ScrollIndicator.defaultProps = {
+};
+
+export default ScrollIndicator;
