1b72cf53e5d407ca46c2b8ea48cd681c403dba20
Zixia Lu <zixialu@gmail.com>
2019-08-14 22:25:36 -0400

    Add copy to about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 
 const About = () => (
   <>
-    <h1 id="about" className="text--xxxl">Zixia Lu</h1>
-    <h1 className="text--xxxl">Web Developer</h1>
+    <h1 id="about" className="text--xxxxl">
+      Zixia Lu is a web developer based in Toronto
+    </h1>
 
-    <p>“And surely none stranger than itself,” echoed the lawyer in the same tones. “For what did Jekyll”--he caught himself up at the word with a start, and then conquering the weakness--“what could Jekyll want with it?” he said.</p>
+    <p className="text--xl">
+      Raised by Saturday morning cartoons and a dialup modem, Zixia knew at an
+      early age that he was destined for great things.
+    </p>
 
-    <p>“You may say that!” said Poole.</p>
-
-    <p>Next they turned to the business table. On the desk, among the neat array of papers, a large envelope was uppermost, and bore, in the doctor’s hand, the name of Mr. Utterson. The lawyer unsealed it, and several enclosures fell to the floor. The first was a will, drawn in the same eccentric terms as the one which he had returned six months before, to serve as a testament in case of death and as a deed of gift in case of disappearance; but in place of the name of Edward Hyde, the lawyer, with indescribable amazement read the name of Gabriel John Utterson. He looked at Poole, and then back at the paper, and last of all at the dead malefactor stretched upon the carpet.</p>
-
-    <p>“My head goes round,” he said. “He has been all these days in possession; he had no cause to like me; he must have raged to see himself displaced; and he has not destroyed this document.”</p>
+    <p className="text--xl">
+      He could not have possibly known what the universe had in store.
+    </p>
   </>
 );
 
