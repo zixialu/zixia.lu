d50f2a8dd43663e2cf7a39107f66f3446d57b654
Zixia Lu <zixialu@gmail.com>
2019-08-17 00:01:03 -0400

    Lint config, update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: `Zixia Lu`,
-    siteUrl: `https://zixia.lu`,
-    description: `Full-stack Developer`,
-    author: `@zixialu`,
+    title: 'Zixia Lu',
+    siteUrl: 'https://zixia.lu',
+    description: 'Full-stack Developer',
+    author: '@zixialu',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'zixia.lu',
+        short_name: 'zixia.lu',
+        description: 'Zixia Lu is a full-stack web developer.',
+        lang: 'en',
+        start_url: '/', // Entry point for PWA
+        background_color: '#e54b4b', // Used on the splash screen on launch
+        theme_color: '#e54b4b', // Toolbar colour
+        display: 'standalone',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-netlify`,
+    'gatsby-plugin-sass',
+    'gatsby-plugin-netlify',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
